e7e48efd99112506f114751a28cfd31d58601ec2
Dan DeGreef <dan.degreef@gmail.com>
2018-11-07 14:28:18 -0600

    add the react display of the photo with login

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import CampaignService from './services/campaign-service';
+import UserService from './services/user-service';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { id: "", photo: "shrug.jpg" };
+
+    // set the priority in the docker-compose file. You will need to restart this app with docker-compose to update it.
+    const priority = process.env.REACT_APP_CAMPAIGN_PRIORITY.split(",").map(Number);
+  console.log(priority);
+    this.campaignService = new CampaignService(priority);
+    this.userService = new UserService();
+
+    this.updateId = this.updateId.bind(this);
+    this.submitId = this.submitId.bind(this);
+  }
+
+  updateId(event) {
+    this.setState({ id: event.target.value });
+  }
+
+  submitId() {
+    const { id } = this.state;
+
+    const user = this.userService.login(Number(id));
+
+    if (!user) {
+      this.setState({ id: "", photo: "shrug.jpg" });
+      return;
+    }
+
+    const photo = this.campaignService.serve(user);
+
+    this.setState({ id: "", photo });
+
+  }
+
   render() {
+    const { id, photo } = this.state;
+
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          Campaign Server
         </header>
+        <p>Log in by entering your User ID and we will show you your campaign!</p>
+        <p><input type='text' value={id} onChange={this.updateId}/> <button onClick={this.submitId}>Login</button></p>
+        <p><img src={`/images/${photo}`} alt="Your Campaign" /></p>
       </div>
     );
   }
